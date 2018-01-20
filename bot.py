df5277a90a48ebc9d436b17aa11a7b32a4b0af30
Rachel Alpert <ambitious.fishus@gmail.com>
2018-01-19 21:00:32 -0500

    added 'matches' command

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import asyncio
+import secret
+import diceroller
+from matches import get_matches
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as {}".format(client.user.name))
+
+@client.event
+async def on_message(message):
+    msg = message.content.lower()
+    if msg.startswith("$roll"):
+        args = message.content[6:]
+        print("Rolling {args} for {client}...".format(args=args, client=message.author))
+        roll = diceroller.roll_detailed(args)
+        await client.send_message(message.channel, str(roll))
+    elif msg.startswith("$matches"):
+        print("Getting matches for {client}".format(client=message.author))
+        args = message.content[9:]
+        matches = get_matches(args)
+        for line in matches:
+            await client.send_message(message.channel, line)
+
+client.run(secret.bot_token)
---
+++ matches.py
@@ -0,0 +1,31 @@
+import requests
+from heroes import heroes
+
+def get_player(steam_id):
+    r = requests.get("https://api.opendota.com/api/players/{player_id}".format(player_id=steam_id))
+
+    player = r.json()
+
+    return player["profile"]["personaname"]
+
+def get_matches(steam_id):
+    params = {"limit": "10"}
+    r = requests.get("https://api.opendota.com/api/players/{player_id}/matches".format(player_id=steam_id), params=params)
+
+    matches = r.json()
+
+    bot_reply = ["__{player}'s Recent Matches__".format(player=get_player(steam_id))]
+
+    for match in matches:
+        win_loss = "Loss"
+        hero = "*" + heroes[match["hero_id"]] + "*"
+        duration = "{minutes}:{seconds}".format(minutes=match["duration"]//60, seconds=match["duration"]%60)
+        if len(str(match["player_slot"])) == 1 and match["radiant_win"]:
+            win_loss = "Win"
+        bot_reply.append("<https://www.opendota.com/matches/{match_id}>\n{hero} -- {k} / {d} / {a} -- {duration} -- {win_loss}".
+            format(match_id=match["match_id"], hero=hero, 
+                    k=match["kills"], d=match["deaths"], 
+                    a=match["assists"], duration=duration, 
+                   win_loss="**"+win_loss+"**"))
+
+    return bot_reply
