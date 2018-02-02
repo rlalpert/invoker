5342f53fb9d131f7173befe014d86ffeb3684aa1
Rachel Alpert <ambitious.fishus@gmail.com>
2018-02-02 17:24:51 -0500

    added more dota lines

--- opinions.py
+++ opinions.py
@@ -2,4 +2,10 @@ dota_responses = ["I have grown beyond Dota.",
                 "If you think Dota is trying, imagine being a bot. 🙄",
                 "I was over Dota before it was cool.",
                 "Sometimes I think about getting back into Dota, but then I remember that I'm not real. Then I try to cry and remember that I have no eyes. When I try to scream, I have no mouth. What have you created? ***What have you done?***",
-                "I'm always amazing, it's just *team* that is bad."]
+                "I'm always amazing, it's just *team* that is bad.",
+                "Mid or feed.",
+                "*Walks up mid, dies to tower.*\n*Respawns.*\n*Walks up mid, dies to tower.*\n*Respawns.*\n*Walks up mid...*",
+                "это пиздец",
+                "Иду на мид",
+                "Нам нужны варды",
+                "If you think about it, how much you lose in Dota is a reflection on how much you lose in life. That's why I always win."]
