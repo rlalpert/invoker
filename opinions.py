97296a0a1175c07e70a4d27cfe634ae63174d5ff
Rachel Alpert <ambitious.fishus@gmail.com>
2018-02-07 16:58:34 -0500

    added more dota opinions

--- opinions.py
+++ opinions.py
@@ -1,5 +1,5 @@
 dota_responses = ["I have grown beyond Dota.", 
-                "If you think Dota is trying, imagine being a bot. 🙄",
+                "If you think Dota is hard, imagine being a bot. 🙄",
                 "I was over Dota before it was cool.",
                 "Sometimes I think about getting back into Dota, but then I remember that I'm not real. Then I try to cry and remember that I have no eyes. When I try to scream, I have no mouth. What have you created? ***What have you done?***",
                 "I'm always amazing, it's just *team* that is bad.",
@@ -8,4 +8,5 @@ dota_responses = ["I have grown beyond Dota.",
                 "это пиздец",
                 "Иду на мид",
                 "Нам нужны варды",
-                "If you think about it, how much you lose in Dota is a reflection of how much you lose in life. That's why I always win."]
+                "If you think about it, how much you lose in Dota is a reflection of how much you lose in life. That's why I always win.",
+                "**LIFE!**\nWe're playing a game where we shout out things you're bad at, right?"]
